92d931249e01c465f87a1ee6762ff9dc06eec9b8
Eddie <skdpwls830@gmail.com>
2022-07-26 23:48:22 +1000

    no complicated, just what the lab wanted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+document.getElementById("submit").addEventListener("click",function(event) {
+  event.preventDefault();
+},false)
+
+function getItem() {
+  const input = document.getElementById("task-item");
+
+  addItem(input);
+}
+
+function addItem(task) {
+  const listNode = document.getElementById("tasks");
+  const node = document.createElement('li');
+  node.innerText = task;
+  listNode.appendChild(node);
+  
+}
